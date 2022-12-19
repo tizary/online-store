6229ffb2da88bdeb7e11c15e19e885151bf533a0
tizary <tizary@mail.ru>
2022-12-19 03:16:11 +0300

    feat: do webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        open: true,
+    },
+    entry: path.resolve(__dirname, 'src', 'index'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: 'index.js',
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'index.css',
+        }),
+        new EslingPlugin({ extensions: 'ts' }),
+    ],
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+};
