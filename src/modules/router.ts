a6b2e4b7755ad1790c619adcbde439b39efd2c64
tizary <tizary@mail.ru>
2023-01-07 16:51:56 +0300

    feat: add redirect

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -49,28 +49,4 @@ export class Router {
     }
 }
 
-// const routes = [
-//     {
-//         path: '/',
-//         data: mainPage,
-//     },
-//     {
-//         path: '/cart',
-//         data: cartPage,
-//     },
-// ];
-
-// const main = document.querySelector('.main');
-
-// function route(event: Event) {
-//     event.preventDefault();
-//     if (event.target instanceof HTMLAnchorElement) {
-//         history.pushState({}, '', event.target.href);
-//     }
-//     const route: Record<string, string> | undefined = routes.find((route) => route.path === window.location.pathname);
-//     if (main && route) {
-//         main.innerHTML = route.data;
-//     }
-// }
-
-// export default route;
+export const router = new Router();
